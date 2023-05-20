388c70bdd7f513843414ea9b1304fffba32fe284
Chaithra Rai - c0r0fga <Chaithra.Rai@walmart.com>
2023-05-20 12:59:14 +0530

    class based component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 
-class App extends React{
+class App extends Component{
   render(){
   return (
     <div className="App">
