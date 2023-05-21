ea727a256c93c6f82bccbc60cd2cef5bbf198f1a
Chaithra Rai - c0r0fga <Chaithra.Rai@walmart.com>
2023-05-21 10:47:42 +0530

    variable and method calls

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import './App.css';
 import React, {Component,Fragment} from 'react';
 
 class App extends Component{
+  func = () => 'function from class'
+  
   render(){
-  return React.createElement('div',{className: 'App'},React.createElement('h1',null,'React code without jsx and pure JS!'));
+    const name='Chai';
+    const foo= () => 'function from method'
+  return <div className='App'>
+    <h1>Access constant {name}</h1>
+    <h1>Access functions here : {foo()}: {this.func()}</h1>
+  </div>;
 }
 }
 
