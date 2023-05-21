6ff9037233e04978b3755f1b8929a6c29c82cc17
Chaithra Rai - c0r0fga <Chaithra.Rai@walmart.com>
2023-05-21 16:14:00 +0530

    introducing Navbar component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import './App.css';
-import React, {Component,Fragment} from 'react';
+import React, {Component} from 'react';
+import Navbar from './components/layout/Navbar';
 
 class App extends Component{
-  
   render(){
-    const name='Chai';
-    const loading=false;
-    const showname=false;
-
-  return(
-  <div className='App'>
-    <h1>The app</h1>
-    {loading?<h4>Loading....</h4>:<h1>Hii {showname && name}</h1>}
-  </div>);
+    return(
+      <div className='App'>
+        <Navbar/>
+      </div>
+    )
 }
 }
 
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,10 @@
+import React,{Component, Fragment} from 'react'
+
+class Navbar extends Component{
+render(){
+    return(<Fragment><h1>Navbar</h1></Fragment>);
+}
+
+}
+
+export default Navbar;
