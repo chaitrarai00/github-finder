6514b6bf75255b850790706d2f92742bb88e5e61
Chaithra Rai - c0r0fga <Chaithra.Rai@walmart.com>
2023-05-21 10:23:43 +0530

    Use Fragment instead of a parent extra div element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import React, {Component} from 'react';
+import React, {Component,Fragment} from 'react';
 
 class App extends Component{
   render(){
   return (
-    <div className="App">
+    <Fragment>
       <h1>After cleanup</h1>
-    </div>
+    </Fragment>
   );
 }
 }
