3454cf33c488bab6834184254437031165b5cee5
Chaithra Rai - c0r0fga <Chaithra.Rai@walmart.com>
2023-05-21 10:56:22 +0530

    conditionals

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import React, {Component,Fragment} from 'react';
 
 class App extends Component{
-  func = () => 'function from class'
   
   render(){
     const name='Chai';
-    const foo= () => 'function from method'
-  return <div className='App'>
-    <h1>Access constant {name}</h1>
-    <h1>Access functions here : {foo()}: {this.func()}</h1>
-  </div>;
+    const loading=false;
+    const showname=false;
+
+  return(
+  <div className='App'>
+    <h1>The app</h1>
+    {loading?<h4>Loading....</h4>:<h1>Hii {showname && name}</h1>}
+  </div>);
 }
 }
 
